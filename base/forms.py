9f2e5c98971075ad4c5542de2babb5dfc4d99949
Boril Shoylekov <borilshoylekov@gmail.com>
2024-04-03 15:06:31 +0300

    first commit

---
+++ base/forms.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, User, Message
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['body', 'image']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
---
+++ izvergs/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for izvergs project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'izvergs.settings')
+
+application = get_asgi_application()
